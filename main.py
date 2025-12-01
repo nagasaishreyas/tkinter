1a3d4d6c56df380c9df7ccf09ad1a1f81f1abf9a
sudheerkuntumalla <sudheerkuntumalla3004@gmail.com>
2025-12-01 20:55:02 +0530

    tkinter

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from datetime import date
+
+root = TK()
+root.title("Getting started with widgths")
+root.geomertry("400x300")
+ibi = Label(text ="hey there! ", fg = "white", bg = "#072SFS", height = 1, widght =300)
+name_ibi = Label(text= "full screen", bg = "#3895D3" )
+name_entry
